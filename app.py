2d26d856a0cd9e296ef36730683a441b250de476
MathiusCp <castillomateous@gmail.com>
2025-11-27 15:19:05 -0500

    aplication

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Página Premium</title>
+    <title>Página Premium total</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Google Fonts -->
